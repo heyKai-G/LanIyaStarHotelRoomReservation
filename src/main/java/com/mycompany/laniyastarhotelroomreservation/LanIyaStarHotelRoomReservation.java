bcc303a657cc9418b1bb6d8052017219cc5bf681
Kaisser Garcia <garcia.kaisser23@gmail.com>
2025-11-08 13:28:17 +0800

    First commit hotel

---
+++ src/main/java/com/mycompany/laniyastarhotelroomreservation/LanIyaStarHotelRoomReservation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laniyastarhotelroomreservation;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class LanIyaStarHotelRoomReservation {
+
+    public static void main(String[] args) {
+        System.out.println("LanIya Star Hotel Room Reservation");
+    }
+}
