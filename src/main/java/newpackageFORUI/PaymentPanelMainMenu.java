8bf5732c2c903da593f7be95c82b6ebe72c311b1
Kaisser Garcia <garcia.kaisser23@gmail.com>
2025-11-13 17:20:30 +0800

    Added 2 file section of the main Payment Panel options (Cash and Card payment)

--- src/main/java/newpackageFORUI/PaymentPanelMainMenu.java
+++ src/main/java/newpackageFORUI/PaymentPanelMainMenu.java
@@ -8,14 +8,14 @@ package newpackageFORUI;
  *
  * @author Lenovo
  */
-public class PaymentPanel extends javax.swing.JFrame {
+public class PaymentPanelMainMenu extends javax.swing.JFrame {
     
-    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(PaymentPanel.class.getName());
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(PaymentPanelMainMenu.class.getName());
 
     /**
      * Creates new form PaymentPanel
      */
-    public PaymentPanel() {
+    public PaymentPanelMainMenu() {
         initComponents();
     }
 
@@ -66,7 +66,7 @@ public class PaymentPanel extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new PaymentPanel().setVisible(true));
+        java.awt.EventQueue.invokeLater(() -> new PaymentPanelMainMenu().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
